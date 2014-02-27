62a84b8b58164b7f104fb6f71acffafdcd9fe42a
Edwin Espaderos <eespaderos@gmail.com>
2014-02-26 22:22:26 -0500

    added router, view, model and updated main.js

---
+++ app/scripts/model.js
@@ -0,0 +1,9 @@
+var Item = Backbone.Model.extend({});
+var ItemsCollection = Backbone.Collection.extend({
+  model: Item,
+  url: "https://api.etsy.com/v2/listings/active.js?api_key=kr9rjq7dc9c24jv6fccq2hus&includes=Images&callback=?",
+ 
+  parse: function(response) {
+    return response.results;
+  }
+})
---
+++ app/scripts/router.js
@@ -0,0 +1,20 @@
+var MainRouter = Backbone.Router.extend({
+  routes: {
+    "shops" : "showShops",
+    "shops/:keyword" : "showShops"
+  },
+ 
+  initialize: function(){
+    this.items = new ItemsCollection;
+    this.items.on('add', function(item){
+      new ItemView({model: item})
+    })
+  },
+ 
+  showShops: function(keyword){
+    if (keyword) {
+      this.items.url = "https://api.etsy.com/v2/listings/active.js?keywords="+ keyword +"&includes=Images&api_key=4vrbm3vo57peni06gsiwgh6b&callback=?"
+    }
+    this.items.fetch();
+  }
+});
